f99f458fade9b559ed702d739f308f86368bc0f2
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-28 23:37:23 +0530

    [Vinay]Created IPLAnalyser project

---
+++ src/test/java/IPLAnalyser/IPLAnalyserTest.java
@@ -0,0 +1,14 @@
+package IPLAnalyser;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IPLAnalyserTest {
+	@Test
+	public void testapi() {
+		assertEquals(1, 1);
+	}
+	
+}
+
