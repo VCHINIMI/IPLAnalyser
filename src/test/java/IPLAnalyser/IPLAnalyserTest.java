22fb9f50c92d5ddf363960cfab4e9fe7f35b9e76
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-30 14:07:32 +0530

    [Vinay]Added: Test Case for checking highest Strike rate amoong Batsmen

--- src/test/java/IPLAnalyser/IPLAnalyserTest.java
+++ src/test/java/IPLAnalyser/IPLAnalyserTest.java
@@ -2,13 +2,30 @@ package IPLAnalyser;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class IPLAnalyserTest {
+	public String IPL_RUNS_FILEPATH = "C:\\Users\\vinay\\Desktop\\myWorkSpace\\IPLAnalyser\\src\\test\\resources\\IPL2019FactsheetMostRuns.csv";
+	public IPLAnalyser iplAnalyser;
+
+	@Before
+	public void intitializeTest() {
+		iplAnalyser = new IPLAnalyser();
+		ExpectedException exceptionRule = ExpectedException.none();
+		exceptionRule.expect(IPLException.class);
+	}
+
+//	Highest Strike Rate = Ishant Sharma
 	@Test
-	public void testapi() {
-		assertEquals(1, 1);
+	public void highestStrikeRateBatsmentest() throws IPLException {
+		iplAnalyser.loadIPLBatsmenData(IPL_RUNS_FILEPATH);
+		List<IPLRunsCSV> batsmenList = iplAnalyser.highestStrikeRate();
+		assertEquals(batsmenList.get(0).playerName, "Ishant Sharma");
 	}
 	
+// 		
 }
-
