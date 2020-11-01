b2f7d2c88ebee424ded53c7e299f3122a4a5aad4
VCHINIMI <vinaychinimilli@gmail.com>
2020-11-01 15:40:18 +0530

    [Vinay]Added: sortByAverageAndStrike method for sorting

--- src/main/java/IPLAnalyser/IPLAnalyser.java
+++ src/main/java/IPLAnalyser/IPLAnalyser.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import CSVBuilder.*;
 
 public class IPLAnalyser {
@@ -31,17 +33,31 @@ public class IPLAnalyser {
 //	COMPARE BATSMEN BY STRIKERATE 
 	public List<IPLRunsCSV> highestStrikeRate() {
 		Comparator<IPLRunsCSV> comparator = Comparator.comparing(Batsmen -> Batsmen.strikeRate);
-		this.batsmenList.sort(comparator);
-		Collections.reverse(batsmenList);
-		return this.batsmenList;
+		return batsmenList.stream().sorted(comparator.reversed()).collect(Collectors.toList());
 	}
 
 //	COMPARE BATSMEN BY BATTING-AVERAGE
 	public List<IPLRunsCSV> sortByBattingAverage() {
 		Comparator<IPLRunsCSV> comparator = Comparator.comparing(Batsmen -> Batsmen.battingAverage);
+		return batsmenList.stream().sorted(comparator.reversed()).collect(Collectors.toList());
+	}
+
+//	COMPARE BY BOUNDARIES (4'S AND 6'S)	
+	public List<IPLRunsCSV> sortByBoundaries() {
+		Comparator<IPLRunsCSV> comparator = Comparator.comparing(Batsmen -> Batsmen.noOfBoundaries + Batsmen.noOfSixes);
 		this.batsmenList.sort(comparator);
 		Collections.reverse(batsmenList);
-		return this.batsmenList;
+		return batsmenList.stream().sorted(comparator.reversed()).collect(Collectors.toList());
+	}
+	
+//	GREAT AVG WITH BEST STRIKE RATE POSSIBLE
+	public List<IPLRunsCSV> sortByAverageAndStrikerate() {
+		Comparator<IPLRunsCSV> averageComparator = Comparator.comparing(Batsmen -> Batsmen.battingAverage);
+		Comparator<IPLRunsCSV> strikerateComparator = Comparator.comparing(Batsmen -> Batsmen.strikeRate);
+		Comparator<IPLRunsCSV> comparator = averageComparator.thenComparing(strikerateComparator);
+		this.batsmenList.sort(comparator);
+		Collections.reverse(batsmenList);
+		return batsmenList.stream().sorted(comparator.reversed()).collect(Collectors.toList());
 	}
 
 //  BUBBLE SORT METHOD 	
@@ -56,5 +72,5 @@ public class IPLAnalyser {
 				}
 			}
 		}
-	}
+	}	
 }
